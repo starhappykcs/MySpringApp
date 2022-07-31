182f8d9fe0d3217ccca0b9a9b1074b7eab03dc7e
HPE <HPE@DESKTOP-G4E9KTO>
2022-07-31 10:31:02 +0900

    first

---
+++ src/main/java/com/example/demo/MySpringAppApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class MySpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringAppApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello, Spring Boot";
+	}
+	
+}
